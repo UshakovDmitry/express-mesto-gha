6a582c259a6ec8d418811ff92fa046e602b5eecb
Dmitry Ushakov <dimau95@yandex.ru>
2023-07-28 21:00:54 +0600

    added fake id

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "",
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
